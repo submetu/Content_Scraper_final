477bbcb4c229f2986a66bd87b7581c2c126e8133
M Subhan Naeem <sub.metu@gmail.com>
2016-10-02 21:23:35 +0300

    Added logs and comments for first time program users so that they choose a suitable time for the scrape to happen in scraper.js

--- scraper.js
+++ scraper.js
@@ -1,3 +1,9 @@
+/*
+IN ORDER TO CHECK THE PROGRAM, CHANGE THE "hour" and "minute" VALUES (on line 23) TO A SUITABLE TIME FOR YOU. THEN RUN THE PROGRAM.
+THE SCRAPE WILL HAPPEN EVERYDAY AT THE CHOSEN "hour" and "minute".
+ */
+
+
 var mkdirp = require('mkdirp'),
 	csv    = require('fast-csv'),
 	fs 	   = require('fs'),
@@ -9,12 +15,14 @@ var date = new Date();
 var dateData = date.getFullYear() + '-' + (date.getMonth() < 10 ? "0" : "") + date.getMonth() + '-' 
 			   +  (date.getDate() < 10 ? "0" : "") + date.getDate();
 
-
+// A log for the user to know what to do if they are starting the scraper.js program without editing the time values on line 23
+console.log(
+"If you are running the program for the first time without opening scraper.js file then you should cancel this session and edit the line 23 of scraper.js. The 'hour' and the 'minute' value should be chosen according to your local time in order to test the program. The program will run everyday at the chosen time.");
 
 //A node job scheduler that runs the scraper everyday at 00:00 hours
-var j = schedule.scheduleJob({hour: 20, minute: 05}, function(){
+	schedule.scheduleJob({hour: 21, minute: 21}, function(){
 
-	console.log('Shirt data saved for today!');
+	console.log("\nShirt data saved for today!");
 	// makes a directory 'data' if it doesn't exist and runs the callback function main.main
 	mkdirp('./data',function main(err){
 		if(err){
@@ -28,7 +36,7 @@ var j = schedule.scheduleJob({hour: 20, minute: 05}, function(){
 				else{
 					var mainArrRemapped=[];
 					//REMAPS THE mainArr SUCH THAT IT IS COMPATIBLE WITH fast-csv MODULE (EXCHANGE ROWS WITH COLUMNS)
-					var mainArrRemapped = mainArr[0].map(function(col, i) {
+	  		        mainArrRemapped = mainArr[0].map(function(col, i) {
 					    return mainArr.map(function(row) {
 					        return row[i];
 					    });
